34cdfb2e251ffb960e9faa20d33e871c4ea17ac4
Burak Ucuncu <burakucuncu@hotmail.com>
2021-01-25 22:41:39 +0300

    4.Gün Odev

---
+++ GenericDictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> sozcukler = new MyDictionary<string>();
+            sozcukler.Add("Almak");
+
+            Console.WriteLine(sozcukler.Lenght);
+
+            sozcukler.Add("Vermek");
+
+            Console.WriteLine(sozcukler.Lenght);
+
+            foreach (var sozcuk in sozcukler.Items)
+            {
+                Console.WriteLine(sozcuk);
+            }
+            //List<string> liste = new List<string>();
+                        
+            //    Console.WriteLine(liste.Count);
+            
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
