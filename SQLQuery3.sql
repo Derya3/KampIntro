3743ab4691d343ba289f757de307ac5436bd13c3
Burak Ucuncu <burakucuncu@hotmail.com>
2021-02-06 15:50:10 +0300

    6.Gün Ödev

---
+++ SQLQuery3.sql
@@ -0,0 +1,5 @@
+﻿Select Products.ProductName As [Ürün Adı], SUM([Order Details].Unitprice*[Order Details].Quantity) As [Kazanılan Toplam Miktar]
+From (([Order Details]
+Inner Join Products On Products.ProductID = [Order Details].ProductID)
+Inner Join Orders On Orders.OrderID = [Order Details].OrderID)
+Group by Products.ProductName
